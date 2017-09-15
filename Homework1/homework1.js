9df34f33734c453c868e51d8ea227dfe4533e43f
JulieL1995 <julie.lelong@hotmail.com>
2017-09-15 17:39:46 +0900

    homework 1 afgewerkt

--- Homework1/homework1.js
+++ Homework1/homework1.js
@@ -1,12 +1,22 @@
     var context = new (window.AudioContext || window.webkitAudioContext)();
 
-    var number_of_samples = 5;
-    var buffers = new Array(number_of_samples); // 0 : kick, 1 : snare, 2 : hihat, 3 : omg, 5 : music
-    var volume_id = new Array("kickVol","snareVol","hihatVol", "omgVol", "musicVol");
-    var volume_label_id = new Array("kickVolLabel","snareVolLabel","hihatVolLabel", "omgVolLabel", "musicVolLabel");
-    var urls = ["hit.mp3","whut.mp3","gasp.mp3","omg.mp3","celtic.mp3"];
+    // beatbox samples
+    var number_of_samples = 4;
+    var buffers = new Array(number_of_samples); // 0 : kick, 1 : snare, 2 : hihat, 3 : omg
+    var volume_id = new Array("kickVol","snareVol","hihatVol", "omgVol");
+    var volume_label_id = new Array("kickVolLabel","snareVolLabel","hihatVolLabel", "omgVolLabel");
+    var urls = ["hit.mp3","whut.mp3","gasp.mp3","omg.mp3"];
     var gain_nodes = new Array(number_of_samples);
 
+    // background music
+    var number_of_songs = 2;
+    var buffer_music = new Array(number_of_songs);
+    var url_music = ["celtic.mp3", "hiphop.mp3"];
+    var gain_node_music = null;
+    var current_song = -1;
+    var current_sourceBuffer = null;
+
+    // stuff for beatbox samples
     for (i  = 0; i < number_of_samples; i++) {
         gain_nodes[i] = context.createGain();
         var vol = document.getElementById(volume_id[i]).value;
@@ -17,7 +27,7 @@
     for (i = 0; i < number_of_samples; i++) {
         (function(i) {
                 var request = new XMLHttpRequest();
-                request.open("Get",urls[i],true);   //  <---- replace this file with yours
+                request.open("Get",urls[i],true);   
                 request.responseType = "arraybuffer";
                 request.onload = function(){
                     context.decodeAudioData(request.response, function(buffer){buffers[i] = buffer;});
@@ -26,6 +36,23 @@
         })(i);
     }
 
+    // stuf for music samples
+    gain_node_music = context.createGain();
+    gain_node_music.gain.value = db2gain(-12);
+    document.getElementById("musicVolLabel").innerHTML = 'Volume:  -12dB'; 
+    
+    for (i = 0; i < number_of_songs; i++) {
+        (function(i) {
+                var request = new XMLHttpRequest();
+                request.open("Get",url_music[i],true);   
+                request.responseType = "arraybuffer";
+                request.onload = function(){
+                    context.decodeAudioData(request.response, function(buffer){buffer_music[i] = buffer;});
+                }
+                request.send();
+        })(i);
+    }    
+    
     window.onload=function(){
         window.addEventListener('keydown', function (key) {
             keyboardDown(key);
@@ -34,6 +61,9 @@
         window.addEventListener('keyup', function (key) {
             keyboardUp(key);
         }, false);
+        
+        var control = document.getElementById("fileChooseInput");
+		control.addEventListener("change", addSong, false);
     }
 
     // plays sound
@@ -116,14 +146,81 @@
         element.dispatchEvent(event);
     }
     
-    var music_source = null;
-    function playmusic() {
-        if (!music_source) {
-            music_source = context.createBufferSource();
-            music_source.buffer = buffers[4];
-            music_source.start();
+    function playmusic(i) {
+            if (current_sourceBuffer) {
+                current_sourceBuffer.disconnect();
+                var song0 = document.getElementById("row" + current_song);
+                song0.className = "clickable-row";
+            }
+            var song1 = document.getElementById("row" + i);
+            song1.className = "active clickable-row";
+            current_song = i;
+            document.getElementById("currentSongPlaying").innerHTML = song1.innerHTML + " is playing";
+            
+            var music_source = context.createBufferSource();
+            music_source.buffer = buffer_music[i];
+            current_sourceBuffer = music_source;
+            music_source.start(0,0);
 	  
-            music_source.connect(gain_nodes[4]);
-            gain_nodes[4].connect(context.destination);
+            music_source.connect(gain_node_music);
+            gain_node_music.connect(context.destination);
+    }
+    
+    function gain2db(gain_value) {
+        var result = 1.0;
+        result = 20.0 * Math.log(gain_value) / Math.log(10.0);
+        return result;
+    } 
+    
+    function volumeUp() {
+        var current_vol_db = gain2db(gain_node_music.gain.value);
+        if (Math.floor(current_vol_db+1) <= 0) {
+            gain_node_music.gain.value = db2gain(current_vol_db + 1);
+            document.getElementById("musicVolLabel").innerHTML = 'Volume:  ' + Math.floor(current_vol_db+1) + 'dB'; 
         }
     }
+    
+    function volumeDown() {
+        var current_vol_db = gain2db(gain_node_music.gain.value);
+        if (Math.floor(current_vol_db-1) >= -24) {
+            gain_node_music.gain.value = db2gain(current_vol_db - 1);
+            document.getElementById("musicVolLabel").innerHTML = 'Volume:  ' + Math.floor(current_vol_db-1) + 'dB';
+        }
+    }
+    
+    function pause() {
+        if (current_sourceBuffer)
+            context.suspend();
+    }
+
+    function play() {
+        if (current_sourceBuffer) {
+            context.resume();
+        }
+    }
+    
+    function replay() {
+        if (current_song >= 0)
+            playmusic(current_song);
+    }
+    
+    function addSong(e) {
+        var file = e.target.files[0];
+        var fileReader = new FileReader();
+        fileReader.onload = fileLoaded;
+        fileReader.readAsArrayBuffer(file);
+        
+        var table = document.getElementById("musicTable");
+        var new_song = table.insertRow(number_of_songs);
+        new_song.innerHTML = "<td>" + document.getElementById("fileChooseInput").value + "</td>";
+        new_song.setAttribute("id", "row"+number_of_songs);
+        new_song.setAttribute("class", "clickable-row");
+        new_song.setAttribute("onclick", "playmusic("+number_of_songs+")");
+        number_of_songs++;
+        document.getElementById("fileChooseInput").value = null;
+    }
+    
+    function fileLoaded(e) {
+        context.decodeAudioData(e.target.result, function(buffer) { buffer_music.push(buffer); });
+    }
+    
