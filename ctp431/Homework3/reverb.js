ccb22c094de49cdb6620ab7582b0c66606cf32a5
JulieL1995 <julie.lelong@hotmail.com>
2017-11-09 16:04:50 +0900

    test

--- ctp431/Homework3/reverb.js
+++ ctp431/Homework3/reverb.js
@@ -1,7 +1,6 @@
 var Reverb = function(context, parameters, delay) {
 
 	this.context = context;
-    //this.input = context.createGain();
     
     // create nodes
     this.reverb = context.createConvolver();
@@ -20,37 +19,19 @@ var Reverb = function(context, parameters, delay) {
     this.wetGain = context.createGain();
     
     // connect
-    //this.input.connect(this.reverb);
     delay.wetGain.connect(this.reverb);
     delay.dryGain.connect(this.reverb);
-    this.reverb.connect(this.wetGain);
+  /*  this.reverb.connect(this.wetGain);
     delay.wetGain.connect(this.dryGain);
     delay.dryGain.connect(this.dryGain);
     
     this.wetGain.gain.value = parameters.reverbWetDry;
-    this.dryGain.gain.value = (1-parameters.reverbWetDry);
+    this.dryGain.gain.value = 1 - parameters.reverbWetDry;
     
     this.dryGain.connect(this.context.destination);
-	this.wetGain.connect(this.context.destination);
-
-    /*
-     var audio = document.getElementById('music');
-    var context = new AudioContext();
-    var source = context.createMediaElementSource(audio);
-    var convolver = context.createConvolver();
-    var irRRequest = new XMLHttpRequest();
-    irRRequest.open("GET", "hall.mp3", true);
-    irRRequest.responseType = "arraybuffer";
-    irRRequest.onload = function() {
-        context.decodeAudioData( irRRequest.response, 
-            function(buffer) { convolver.buffer = buffer; } );
-    }
-    irRRequest.send();
-// note the above is async; when the buffer is loaded, it will take effect, but in the meantime, the sound will be unaffected.
-
-    source.connect( convolver );
-    convolver.connect( context.destination );
-     */
+	this.wetGain.connect(this.context.destination);*/
+  
+    this.reverb.connect(this.context.destination);
 
 	this.parameters = parameters;
 }
