bec4bb6350fcb52658303764dab589a7e8ab5c09
JulieL1995 <julie.lelong@hotmail.com>
2017-11-09 16:09:13 +0900

    test

--- ctp431/Homework3/reverb.js
+++ ctp431/Homework3/reverb.js
@@ -1,6 +1,7 @@
 var Reverb = function(context, parameters, delay) {
 
 	this.context = context;
+    this.input = context.createGain();
     
     // create nodes
     this.reverb = context.createConvolver();
@@ -19,8 +20,9 @@ var Reverb = function(context, parameters, delay) {
     this.wetGain = context.createGain();
     
     // connect
-    delay.wetGain.connect(this.reverb);
-    delay.dryGain.connect(this.reverb);
+    delay.wetGain.connect(this.input);
+    delay.dryGain.connect(this.input);
+    this.input.connect(this.reverb);
   /*  this.reverb.connect(this.wetGain);
     delay.wetGain.connect(this.dryGain);
     delay.dryGain.connect(this.dryGain);
