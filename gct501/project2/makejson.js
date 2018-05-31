ca62f2f31e9446d3fb327528ba2b84e51dd64a65
JulieL1995 <julie.lelong@hotmail.com>
2018-05-31 17:57:27 +0900

    project 2 gct501

---
+++ gct501/project2/makejson.js
@@ -0,0 +1,52 @@
+var urls = ["json/weather.json","json/air.json"]; 
+var content = "{";
+
+for (i = 0; i < urls.length; i++) {
+        (function(i) {
+                var request = new XMLHttpRequest();
+                request.overrideMimeType("application/json");
+                
+                request.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        var response = JSON.parse(this.responseText);
+                        if (i == 0) 
+                            parseWeather(response);
+                        else
+                            parseAir(response);
+                        
+                        if (i == urls.length - 1) {
+                            var http = new XMLHttpRequest();
+                            //http.overrideMimeType("text/plain");
+                            var url = "writejson.php";
+                            var params = "array=" + encodeURIComponent(content);
+                        
+                            http.open('POST', url, true);
+    
+                            http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                            httpc.setRequestHeader("Content-Length", params.length);
+                            http.setRequestHeader("Connection", "close");
+                            
+                            http.onreadystatechange = function() {//Call a function when the state changes.
+                                if(http.readyState == 4 && http.status == 200) {
+                                    // do something
+                                }
+                            }
+                            http.send(params);
+                        }
+                    }
+                }
+                request.open("GET",urls[i],true);  
+                request.send();
+        })(i);
+}
+
+
+
+function parseWeather(arr) {
+    content += arr["WeatherIcon"] + ",";
+    document.getElementById("test").innerHTML = arr["WeatherIcon"];
+}
+function parseAir(arr) {
+    content += arr["Ozone"] + "}";
+    document.getElementById("test").innerHTML += " - " + arr["Ozone"];
+}
