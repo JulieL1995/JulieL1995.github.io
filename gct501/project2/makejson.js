0058e9d80d692f5aece4c3c09fcf9d8c464e138d
JulieL1995 <31945925+JulieL1995@users.noreply.github.com>
2018-06-01 10:42:30 +0900

    Update makejson.js

--- gct501/project2/makejson.js
+++ gct501/project2/makejson.js
@@ -23,13 +23,13 @@ for (i = 0; i < urls.length; i++) {
                             http.open('POST', url, true);
     
                             http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                            http.setRequestHeader("Content-Length", params.length);
-                            http.setRequestHeader("Connection", "close");
+                            //http.setRequestHeader("Content-Length", params.length);
+                            //http.setRequestHeader("Connection", "close");
                             
                             http.onreadystatechange = function() {//Call a function when the state changes.
                                 if(http.readyState == 4 && http.status == 200) {
                                     // do something
-                                    alert("done");
+                                    window.alert("done");
                                 }
                             }
                             http.send(params);
